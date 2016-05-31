e2530bfbf49f9187a46cae8cdfd2b0c018c22a5e
Won Steinbach <wonstein@gmail.com>
2016-05-31 15:02:14 -0400

    Rewrote main()

--- app.py
+++ app.py
@@ -2,24 +2,22 @@ from flask import Flask, request, redirect, render_template
 from bokeh.plotting import figure
 from bokeh.embed import components
 
-api_url = 'https:/www.quandl.com/api/vq/datasets/WIKI/%s.json' % stock
-session = requests.Session()
-session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-raw_data = session.get(api_url)
-
-plot = figure(tools=TOOLS,
-              title='Data from Quandle WIKI set',
-              x_axis_label='date',
-              x_axis_type='datetime')
-
-script, div = components(plot)
-return render_template('graph.html', script=script, div=div)
-
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return render_template('index.html')
+  api_url = 'https:/www.quandl.com/api/vq/datasets/WIKI/%s.json' % stock
+  session = requests.Session()
+  session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+  raw_data = session.get(api_url)
+
+  plot = figure(tools=TOOLS,
+              title='Data from Quandle WIKI set',
+              x_axis_label='date',
+              x_axis_type='datetime')
+
+  script, div = components(plot)
+  return render_template('graph.html', script=script, div=div)
 
 if __name__ == '__main__':
   app.run(port=33507)

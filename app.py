eca033ade408b2c1d3e640dc53f6946ea5b4f03e
Won Steinbach <wonstein@gmail.com>
2016-05-31 14:23:37 -0400

    Demo

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, Render_Template, request, redirect
+import request
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+  return "Hello World! ws"
 
 @app.route('/index')
 def index():

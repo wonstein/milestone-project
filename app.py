71b933b9f62efade37bd8e4097fac607751ad7af
wonstein <wonstein@gmail.com>
2016-06-01 09:58:50 -0700

    Fixed typos

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 from dateutil.relativedelta import relativedelta
 import pandas as pd
 
-
+app = Flask(__name__)
 
 # Accept inputs
 
@@ -16,8 +16,8 @@ def main():
   # Fetch stock data
   start_date = datetime.date.today() - relativedelta(days=5)
   end_date = datetime.date.today()
-  print start_date
-  print type(datetime.date.today())
+  #print start_date
+  #print type(datetime.date.today())
   stock = "AAPL"
 
   api_url = "https://www.quandl.com/api/v3/datasets/WIKI/%s.json?api_key=AbHy5cdz65R3V9XKLexS&start_date=%s" % (stock, str(start_date))
@@ -28,16 +28,16 @@ def main():
 
   # Store data
   data = json.loads(r.text)
-  print json.dumps(data, indent=4)
-  print json.dumps(data['dataset']['column_names'], indent=4)
-  print type(data['dataset']['column_names'])
+  #print json.dumps(data, indent=4)
+  #print json.dumps(data['dataset']['column_names'], indent=4)
+  #print type(data['dataset']['column_names'])
   df = pd.DataFrame(data=data['dataset']['data'], columns=data['dataset']['column_names'])
-  print df.iloc[0:3,1]
+  #print df.iloc[0:3,1]
 
   # Build plot
   output_file("graph2.html")
-  print pd.to_datetime(df['Date'])
-  print type(pd.to_datetime([1, 2]))
+  #print pd.to_datetime(df['Date'])
+  #print type(pd.to_datetime([1, 2]))
 
   p = figure(title="%s: %s - %s" % (stock, start_date.strftime("%B %d, %Y"), end_date.strftime("%B %d, %Y")), x_axis_type='datetime')
   p.xaxis.axis_label = 'Date'
@@ -45,27 +45,8 @@ def main():
   #p.title_text_font_size = '12pt'
   p.line(pd.to_datetime(df['Date']), y=df['Open'], line_width=2)
   
-  script, div = components(plot)
+  script, div = components(p)
   return render_template('graph.html', script=script, div=div)
 
-if __name == '__main__':
+if __name__ == '__main__':
   app.run(port=33507)
-
-
-# @app.route('/')
-# def main():
-#   api_url = 'https:/www.quandl.com/api/vq/datasets/WIKI/%s.json' % stock
-#   session = requests.Session()
-#   session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-#   raw_data = session.get(api_url)
-
-#   plot = figure(tools=TOOLS,
-#               title='Data from Quandle WIKI set',
-#               x_axis_label='date',
-#               x_axis_type='datetime')
-
-#   script, div = components(plot)
-#   return render_template('graph.html', script=script, div=div)
-
-# if __name__ == '__main__':
-#   app.run(port=33507)
